da958030575296b1203de71737a1f21fbf57cb3d
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2023-02-06 19:37:53 -0300

    salva targetcifra na memória

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
-const listenMessageFromContent = (message) => {
-    console.log("Cifra recebida:", message?.targetcifra?.title)
+
+const openPage = () => {
+    console.log("abrindo página")
 }
 
-browser.runtime.onMessage.addListener(listenMessageFromContent);
+const onError = (err) => console.error(err)
+
+// salva targetcifra na memória
+const storeTarget = (targetcifra) => {
+    let storingNote = browser.storage.local.set({ "targetcifra": targetcifra });
+    storingNote.then(openPage, onError)
+}
+
+// aciona quando uma menssagem é recebida do content
+const trigger = (message) => {
+    console.log("Cifra recebida:", message?.targetcifra?.title)
+    if (message?.targetcifra) storeTarget(message?.targetcifra)
+    else console.error("[message.targetcifra] não existe:", message)
+}
+
+browser.runtime.onMessage.addListener(trigger);
