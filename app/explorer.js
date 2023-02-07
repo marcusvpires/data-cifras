4542c2286426078eef16b17c6fa6817ced188ca6
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2023-02-07 00:19:11 -0300

    createPlaylist

---
+++ app/explorer.js
@@ -0,0 +1,43 @@
+const createUniqueID = (string) => {
+    return (string + new Date().toTimeString() + new Date().getMilliseconds()).replace(/[^A-Z0-9]/ig, "");
+}
+
+const onSusses = (message = "sucesso") => console.info(message)
+
+class Musica {
+    constructor(title, author, code) {
+        this[createUniqueID(title)] = {
+            title: title,
+            author: author,
+            code: code,
+            date: new Date()
+        }
+    }
+}
+
+class Playlist {
+    constructor(name) {
+        this.name = name;
+        this.musicas = []
+    }
+    get ID() {
+        return  this.createID()
+    }
+    createID() {
+        return createUniqueID(this.name)
+    }
+}
+
+const createPlaylist = (title, author, code) =>
+    browser.storage.local.get("playlists").then((result) => {
+        let playlists = {}
+        if (result?.playlists) playlists = result.playlists
+        
+        const playlist = new Playlist(title, author, code)
+        playlists[playlist.ID] = playlist
+        console.log(playlists)
+        browser.storage.local.set({"playlists": playlists}).then(onSusses, onError)
+    }, onError)
+
+const getAllPlaylists = (title, author, code) =>
+    browser.storage.local.set(new Musica(title, author, code)).then(onSusses, onError)
