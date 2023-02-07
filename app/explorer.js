91376ce0948ebb41625b9a364993745e39cd04d2
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2023-02-07 00:58:48 -0300

    cria nova playlist

--- app/explorer.js
+++ app/explorer.js
@@ -1,8 +1,16 @@
+/* -------------------------------------------------------------------------- */
+/*                                   assets                                   */
+/* -------------------------------------------------------------------------- */
+
 const createUniqueID = (string) => {
     return (string + new Date().toTimeString() + new Date().getMilliseconds()).replace(/[^A-Z0-9]/ig, "");
 }
 
 const onSusses = (message = "sucesso") => console.info(message)
+
+/* -------------------------------------------------------------------------- */
+/*                                   classes                                  */
+/* -------------------------------------------------------------------------- */
 
 class Musica {
     constructor(title, author, code) {
@@ -28,6 +36,10 @@ class Playlist {
     }
 }
 
+/* -------------------------------------------------------------------------- */
+/*                             Middleware playlist                            */
+/* -------------------------------------------------------------------------- */
+
 const getAllPlaylists = (callback = console.log) =>
     browser.storage.local.get("playlists").then((result) => {
         let playlists = {}
@@ -35,12 +47,12 @@ const getAllPlaylists = (callback = console.log) =>
         callback(playlists)
     }, onError)
 
-const createPlaylist = (name, musicas = []) =>
+const createPlaylist = (name, musicas = [], callback = () => { }) =>
     getAllPlaylists((playlists) => {
         const playlist = new Playlist(name, musicas)
         playlists[playlist.ID] = playlist
         console.log(playlists)
-        browser.storage.local.set({ "playlists": playlists }).then(onSusses, onError)
+        browser.storage.local.set({ "playlists": playlists }).then(callback, onError)
     })
 
 const deletePlaylist = (ID) =>
@@ -54,3 +66,47 @@ const updatePlaylist = (ID, name, musicas = []) =>
         playlists[ID] = new Playlist(name, musicas)
         browser.storage.local.set({ "playlists": playlists }).then(onSusses, onError)
     })
+
+
+/* -------------------------------------------------------------------------- */
+/*                               handle playlist                              */
+/* -------------------------------------------------------------------------- */
+
+const updateComponent = () => {
+    getAllPlaylists((playlists) => {
+        document.getElementById("playlist-tbody").innerText = ""
+        
+        Object.entries(playlists).map(([ID, playlist]) => {
+            const tr = document.createElement("tr")
+            tr.id = ID
+
+            const icon = document.createElement("td")
+            icon.setAttribute("data-label", "icon")
+            icon.style.width = "20px"
+            
+            const img = document.createElement("img")
+            img.classList.add("folder-icon")
+            img.src = "public/folder.png"
+            icon.appendChild(img)
+
+            const name = document.createElement("td")
+            name.innerText = playlist?.name
+            name.setAttribute("data-label", "name")
+            
+            tr.appendChild(icon)
+            tr.appendChild(name)
+            document.getElementById("playlist-tbody").appendChild(tr)
+        })
+    })
+}
+
+const handleNovaPlaylist = () => {
+    const name = window.prompt("Digite o nome da playlist")
+    createPlaylist(name, [],  updateComponent)
+}
+
+const createEvents = () => {
+    document.getElementById("novaPlaylist").addEventListener("click", handleNovaPlaylist)
+}
+
+createEvents()
